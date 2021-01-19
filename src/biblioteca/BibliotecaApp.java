11358479d15dc553ea487869d8287af78f025d91
marcelo <marcelo.velasquez@synchro.com.br>
2021-01-19 17:14:46 -0300

    Criada a classe empréstimo

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,6 +5,14 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       Livro l =  new Livro();     
+      
+      Usuario u = new Usuario();
+      
+      Emprestimo emprestimo = new Emprestimo();
+      
+      
+      
+      
 	}
 
 }
